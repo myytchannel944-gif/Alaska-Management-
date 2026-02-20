e61c83b7aa3e498f6ed96061d9e8c2601c2962f8
myytchannel944-gif <myytchannel944@gmail.com>
2026-02-20 00:35:30 -0500

    Delete types.d.ts

--- types.d.ts
+++
@@ -1,8 +0,0 @@
-declare module './types' {
-    export interface BotConfig {
-        logChannel: string | null;
-        staffRole:   string | null;
-        iaRole:      string | null;
-        mgmtRole:    string | null;
-    }
-}
